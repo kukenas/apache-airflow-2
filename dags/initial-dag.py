1f143a8bea0f63e66e02824e842ded575dfc65dc
Kukenas, Aurimas <kukenas.aurimas@gmail.com>
2022-05-22 21:38:26 +0300

    Minor updates

--- dags/initial-dag.py
+++ dags/initial-dag.py
@@ -7,7 +7,7 @@ def print_hello():
     return 'Hello world from first Airflow DAG!'
 
 dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
+          schedule_interval='30 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
